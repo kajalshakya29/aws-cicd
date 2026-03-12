fac233dfe314319d92a7c15720b65bf425280684
“kajal <kshakya877@gmail.com>
2026-03-12 20:44:41 +0530

    one fake commit

--- src/main/java/com/aws/controller/CourseController.java
+++ src/main/java/com/aws/controller/CourseController.java
@@ -62,9 +62,6 @@ public class CourseController {
 //        return "UP";
 //    }
 
-
-
-
     @GetMapping("/welcome")
     public String greetings() {
         return "Hello Techie , AWS CICD Example working fine !";
